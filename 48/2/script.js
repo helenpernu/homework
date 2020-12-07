162d7f14388eeec134357fafb02c770b116f71fa
Helen <helen.pernu@skola.botkyrka.se>
2020-12-07 09:39:46 +0100

    Vecka48-2

--- 48/2/script.js
+++ 48/2/script.js
@@ -1,6 +1,11 @@
 // Skriv en while loop
 // Som varje iteration (repetition) slumpar ett heltal mellan 0 och 10
 // Om talet blir 6, avsluta loopen
+let number = 0
+while (number != 6){
+    number = Math.floor(Math.random()*11) 
+    console.log(number)
+}
 
 
 // Exempel på output:
